fe35efc7abd3ccc87a1bf323c4684559008e3d1f
spiny006 <angeljworld@hotmail.com>
2026-02-15 14:39:45 -0600

    Agregamos elemento a la libreria, métodos de HTTP

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,25 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Principal {
     static void main() {
+        String cambio = "USD";
 
+        String clave = "b09aaf580ca32c7b9daf785e";
+        String direccion = "https://v6.exchangerate-api.com/v6/" +
+                clave + "/latest/" + cambio;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(direccion))
+                .build();
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
